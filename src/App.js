31869f49dddfcb67a4c04cd55cb3e4f1b8f1fc8c
leon matias roslin <leonmatias1991@gmail.com>
2021-05-12 23:43:25 -0500

    subiendo cambios finales

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, {Fragment, useEffect, useState} from 'react';
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoNoticias from "./components/ListadoNoticias";
+
+
+
+
+function App() {
+
+    //definir la categoria y noticias
+    const [categoria,guardarCategoria] = useState('');
+    const [noticias,guardarNoticias] = useState([]);
+
+    useEffect(() =>{
+        const consultarAPI = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=4b108cc47c484cbc820bfb808fa53e16`;
+
+            const respuesta = await fetch(url);
+            const noticias = await respuesta.json();
+
+            guardarNoticias(noticias.articles);
+        };
+        consultarAPI();
+    },[categoria]);
+
+  return (<Fragment>
+          <Header titulo="Buscador de Noticias"/>
+
+          <div className="container white">
+            <Formulario
+                guardarCategoria = {guardarCategoria}
+            />
+            <ListadoNoticias noticias={noticias}/>
+          </div>
+      </Fragment>
+
+  );
+}
+
+export default App;
